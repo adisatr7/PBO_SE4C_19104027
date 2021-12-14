5440aa95d06f82350110f0734691c39058734e0c
RaddOx7 <19104027@ittelkom-pwt.ac.id>
2021-12-14 15:12:46 +0700

    Modul8

---
+++ src/com/raddox7/pbo/modul8/VMIDemo.java
@@ -0,0 +1,11 @@
+package com.raddox7.pbo.modul8;
+
+public class VMIDemo {
+    public static void main(String[] args) {
+        Salary s = new Salary("Wahyu", "KUBAR", 3, 5_000);
+        s.checkMail();
+
+        Employee e = new Salary("Asep", "JABAR", 4, 2_500);
+        e.checkMail();
+    }
+}
